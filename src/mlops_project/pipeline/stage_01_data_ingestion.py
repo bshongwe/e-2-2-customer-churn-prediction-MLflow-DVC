dc8c07f4cebca12d4a1053b80e5e52395decbad9
Ernest Bhekizwe Shongwe <126109055+bshongwe@users.noreply.github.com>
2025-02-12 00:50:44 +0200

    Create stage_01_data_ingestion.py

---
+++ src/mlops_project/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,32 @@
+import sys
+from mlops_project.config.configuration import ConfigurationManager
+from mlops_project.components.data_ingestion import DataIngestion
+from mlops_project.logger import logging
+from mlops_project.exception import MyException
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            data_ingestion_config = config.get_data_ingestion_config()
+            data_ingestion = DataIngestion(config=data_ingestion_config)
+            data_ingestion.download_file()
+            data_ingestion.extract_zip_file()
+        except Exception as e:
+            raise MyException(e, sys)
+        
+if __name__ == '__main__':
+    try:
+        logging.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logging.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        raise MyException(e, sys)
+
+        
